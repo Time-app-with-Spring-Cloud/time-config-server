d42cf237bce185c1c4b97d4014f06d0340c8a56e
Pedro Galan <pedro.galan@piksel.com>
2020-06-10 07:31:28 +0100

    Spring boot app skeleton

---
+++ src/main/java/es/pedrogalan/time/configserver/TimeConfigServerApplication.java
@@ -0,0 +1,13 @@
+package es.pedrogalan.time.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeConfigServerApplication.class, args);
+	}
+
+}
